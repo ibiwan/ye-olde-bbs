95f581bacfc08c898d561f07ae7f8c41a29793a7
Jeremy Kent <jkent@intuit.com>
2022-01-21 22:29:52 -0800

    display one user's data

--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -1,9 +1,18 @@
 import { UsersList } from "./styles";
 
 export default function Users(props) {
+    const user = {
+        id: 18,
+        name: "Maverick"
+    }
+
     return (
         <div style={UsersList}>
             <h1>USERS:</h1>
+            <div>
+                <div>Id: {user.id}</div>
+                <div>Name: {user.name}</div>
+            </div>
         </div>
     )
 }
