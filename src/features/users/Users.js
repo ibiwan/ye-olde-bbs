ba5efc766a3eacc075b81a0b4ac7978bdd54397c
Jeremy Kent <jkent@intuit.com>
2022-01-21 22:35:48 -0800

    display multiple users by mapping with User objects

--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -2,18 +2,34 @@ import { UsersList } from "./styles";
 import User from "./User";
 
 export default function Users(props) {
-    const user = {
-        id: 18,
-        name: "Maverick",
-        email: "mav@topgun.mil",
-        gender: "male",
-        status: "active",
-    }
+    const users = [
+        {
+            id: 18,
+            name: "Maverick",
+            email: "mav@topgun.mil",
+            gender: "male",
+            status: "active",
+        },
+        {
+            id: 19,
+            name: "Goose",
+            email: "gosling@topgun.mil",
+            gender: "male",
+            status: "inactive",
+        },
+    ]
+
+    const userList = users.map((user, i) =>
+        <User
+            user={user}
+            key={`${i}.${user.id}`}
+        />
+    )
 
     return (
         <div style={UsersList}>
             <h1>USERS:</h1>
-            <User user={user} />
+            {userList}
         </div>
     )
 }
