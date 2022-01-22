c8a57ee4e613255835f0a67a21ef76ac2c2ebd90
Jeremy Kent <jkent@intuit.com>
2022-01-22 00:59:55 -0800

    fix selected-user posts request by adding api key

--- src/datasources/gorest.js
+++ src/datasources/gorest.js
@@ -2,10 +2,16 @@
 
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+const API_KEY = process.env.REACT_APP_GOREST_API_KEY
+
 export const gorestApi = createApi({
     reducerPath: 'gorestApi',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://gorest.co.in/public/v1',
+        prepareHeaders: headers => {
+            headers.set("Authorization", `Bearer ${API_KEY}`)
+            return headers
+        }
     }),
     endpoints: builder => ({
         getUsers: builder.query({
