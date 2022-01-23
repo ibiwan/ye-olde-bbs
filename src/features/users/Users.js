7e7ef332ef8156e1f5b7a7b4f36adcc330eeb152
Jeremy Kent <jkent@intuit.com>
2022-01-22 17:02:02 -0800

    animate user list reorderinf

--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Flipper, Flipped } from 'react-flip-toolkit'
+
 import { useGetUsersQuery } from "../../datasources/gorest";
 import { UsersList } from "./styles";
 import User from "./User";
-import { populateUserList, selectUsers } from "./usersSlice";
+import { populateUserList, selectSelectedUserId, selectUsers } from "./usersSlice";
 
 export default function Users(props) {
     const { data, error, isLoading } = useGetUsersQuery()
@@ -15,20 +17,33 @@ export default function Users(props) {
         }
     }, [isLoading, data])
 
+    const selectedUserId = useSelector(selectSelectedUserId)
 
-    const users = useSelector(selectUsers)
+    let users = [...useSelector(selectUsers)]
+    users.sort((a, b) => {
+        if (a.id === selectedUserId) return -1
+        if (a < b) return -1
+        if (a > b) return 1
+        return 0
+    })
 
     const userList = users.map((user, i) =>
-        <User
-            user={user}
-            key={`${i}.${user.id}`}
-        />
+        <Flipped
+            key={`${user.id}`}
+            flipId={`${user.id}`}
+        >
+            <div>
+                <User user={user} />
+            </div>
+        </Flipped>
     )
 
     return (
         <div style={UsersList}>
             <h1>USERS:</h1>
-            {userList}
+            <Flipper flipKey={selectedUserId}>
+                {userList}
+            </Flipper>
         </div>
     )
 }
