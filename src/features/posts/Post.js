9895549f1268685fe1d97f2cdae4d1f432959ef5
Jeremy Kent <jkent@intuit.com>
2022-01-22 17:06:58 -0800

    select user by clicking on a post

--- src/features/posts/Post.js
+++ src/features/posts/Post.js
@@ -1,16 +1,20 @@
 import * as React from 'react'
-import { useSelector } from 'react-redux'
-import { selectSelectedUserId, selectUserById } from '../users/usersSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectSelectedUserId, selectUserById, selectUserId } from '../users/usersSlice'
 import { PostCard, UserPostCard } from './styles'
 
 export default function Post({ post: { id, user_id, title, body } }) {
-
+    const dispatch = useDispatch()
     const selectedUserId = useSelector(selectSelectedUserId)
     const user = useSelector(selectUserById(user_id))
 
     return (
         <div
             style={selectedUserId === user_id ? UserPostCard : PostCard}
+            onClick={(e) => {
+                dispatch(selectUserId(user_id))
+                e.stopPropagation()
+            }}
         >
             <div style={{ fontWeight: "normal" }}>User: {user ? user.name : user_id}</div>
             <br />
